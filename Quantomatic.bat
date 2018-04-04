f1336fa6e93e54fb11a2f55a06b9fd043c39d77b
assombrance <assombrance.anderson@gmail.com>
2018-04-04 13:42:29 +0200

    no more conflict between sbt run and sbt assembly

--- Quantomatic.bat
+++ Quantomatic.bat
@@ -1,1 +1,2 @@
-java -jar scala/target/scala-2.11/Quantomatic.jar
+cd scala
+java -jar ../scala/target/scala-2.11/Quantomatic.jar
--- Quantomatic.sh
+++ Quantomatic.sh
@@ -1,2 +1,3 @@
 chmod +x scala/target/scala-2.11/Quantomatic.jar
-./scala/target/scala-2.11/Quantomatic.jar
+cd scala
+java -jar ../scala/target/scala-2.11/Quantomatic.jar
