6be10cd4faa3e4178d0ad1df8da48b189f3edb89
assombrance <assombrance.anderson@gmail.com>
2018-05-31 11:42:31 +0200

    code cleaned an input can now be called "i", "in" or "input" in Quantomatic similarly, an output can now be called "o", "out" or "output"

--- src/numbered_annotations.py
+++ src/numbered_annotations.py
@@ -1,39 +1,34 @@
-import json
+# encoding=UTF-8
+"""
+Preparation step, checks if I/O order can be deduced without user being required to input anything
+"""
 import sys
 import numpy as np
 
+import loader
 
 if __name__ == '__main__':
     diagram_file_path = sys.argv[1]
-    file = open(diagram_file_path)
-    content_string = file.read()
-    diagram_dictionary = json.loads(content_string)
 
-    if 'wire_vertices' in diagram_dictionary:
-        wire_vertices_dictionary = diagram_dictionary['wire_vertices']
-    else:
-        wire_vertices_dictionary = []
-    if 'undir_edges' in diagram_dictionary:
-        undir_edges_dictionary = diagram_dictionary['undir_edges']
-    else:
-        undir_edges_dictionary = []
+    wne_dir = loader.load_dictionaries(diagram_file_path)
+    wires, nodes, edges = loader.dictionary_to_data(*wne_dir)
+
+    input_names = {"input", "in", "i"}
+    output_names = {"output", "out", "o"}
     all_numbered = True
     inputs_order_dic = {}
     outputs_order_dic = {}
 
-    for node_name in wire_vertices_dictionary:
+    for wire in wires:
         node_found_and_numbered = False
-        for edge_name in undir_edges_dictionary:
-            if dict(undir_edges_dictionary[edge_name])['tgt'] == node_name or \
-                    dict(undir_edges_dictionary[edge_name])['src'] == node_name:
-                if 'data' in undir_edges_dictionary[edge_name] and \
-                        'label' in dict(undir_edges_dictionary[edge_name])['data']:
-                    if 'input' in dict(undir_edges_dictionary[edge_name])['data']['label']:
-                        inputs_order_dic[node_name] = dict(undir_edges_dictionary[edge_name])['data']['label']
-                        node_found_and_numbered = True
-                    elif 'output' in dict(undir_edges_dictionary[edge_name])['data']['label']:
-                        outputs_order_dic[node_name] = dict(undir_edges_dictionary[edge_name])['data']['label']
-                        node_found_and_numbered = True
+        for edge in edges:
+            if wire in edge:
+                if input_names.intersection(edge.label):
+                    inputs_order_dic[wire.name] = edge.label
+                    node_found_and_numbered = True
+                elif output_names.intersection(edge.label):
+                    outputs_order_dic[wire.name] = edge.label
+                    node_found_and_numbered = True
                 else:
                     all_numbered = False
         if not node_found_and_numbered:
@@ -45,8 +40,9 @@ if __name__ == '__main__':
     if all_numbered:
         for i in np.arange(len(inputs_order_dic)):
             node_found = False
+            input_names_numbered = {input_name + str(i) for input_name in input_names}
             for node_name in inputs_order_dic:
-                if inputs_order_dic[node_name] == "input" + str(i):
+                if inputs_order_dic[node_name] in input_names_numbered:
                     inputs_order_list.append(node_name)
                     node_found = True
             if not node_found:
@@ -54,8 +50,9 @@ if __name__ == '__main__':
                 break
         for i in np.arange(len(outputs_order_dic)):
             node_found = False
+            output_names_numbered = {output_name + str(i) for output_name in output_names}
             for node_name in outputs_order_dic:
-                if outputs_order_dic[node_name] == "output" + str(i):
+                if outputs_order_dic[node_name] in output_names_numbered:
                     outputs_order_list.append(node_name)
                     node_found = True
             if not node_found:
